c5c21b32d285ad44050ae05f30f044b4e8cb0c24
aloner778 <aloner8888@gmail.com>
2020-06-22 14:31:37 +0900

    my first react hook with useState

--- src/App.js
+++ src/App.js
@@ -1,9 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function App() {
+  const [count, setCount] = useState(0)
+
+  const increment = () => {
+    setCount(prevCount => prevCount + 1)
+  }
+  const increment2 = () => {
+    for (let i = 0; i < 100; i++) {
+      setCount(prevCount => prevCount + 1)
+    }
+  }
+  const decrement = () => {
+    setCount(prevCount => prevCount - 1)
+  }
+
+  const double = () => {
+    setCount(prevCount => prevCount * 2)
+  }
+
+  const three = () => setCount(prevCount => (
+    prevCount % 3 === 0 ? prevCount / 3 : prevCount
+  ))
+
   return (
     <React.Fragment>
-      
+      <p>count: {count}</p>
+      <button onClick={increment}>+</button>
+      <button onClick={() => setCount(prevCount => prevCount + 10)}>++</button>
+      <button onClick={increment2}>+++</button>
+      <button onClick={decrement}>-</button>
+      <button onClick={() => setCount(prevCount => prevCount - 10)}>--</button>
+
+      <button onClick={double}>*</button>
+      <button onClick={three}>-3-</button>
     </React.Fragment>
   );
 }
