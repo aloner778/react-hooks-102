fe81d3946d82015ffa05aa36794eac6a94ff0882
aloner778 <aloner8888@gmail.com>
2020-06-23 10:07:06 +0900

    result window.confirm(message)

--- src/components/Event.js
+++ src/components/Event.js
@@ -2,7 +2,8 @@ import React from 'react'
 
 function Event({ event, dispatch }) {
   const handleClickDeleteButton = () => {
-    dispatch({ type: 'DELETE_EVENT', id: event.id })
+    const result = window.confirm(`イベント（id=${event.id}）を本当に削除しても良いですか？`)
+    if (result) dispatch({ type: 'DELETE_EVENT', id: event.id })
   }
 
   return (
