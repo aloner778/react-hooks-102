b52dbdc7218a94e90d2a045e2a9c59a67d67eaf0
aloner778 <aloner8888@gmail.com>
2020-06-23 15:15:20 +0900

    implement localStrage

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React, { useReducer } from 'react'
+import React, { useReducer, useEffect } from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import reducer from '../reducers'
 import Events from './Events'
@@ -7,11 +7,17 @@ import AppContext from '../contexts/AppContext'
 import OperationLogs from './OperationLogs'
 
 function App() {
-  const initialState = {
+  const appState = localStorage.getItem('appWithRedux')
+  const initialState = appState ? JSON.parse(appState) : {
     events: [],
     operationLogs: []
   }
   const [state, dispatch] = useReducer(reducer, initialState)
+
+  useEffect(() => {
+    //localStorageは文字列を扱うため、json.stringify()で文字列に変換する必要がある
+    localStorage.setItem('appWithRedux', JSON.stringify(state))
+  }, [state])
 
   return (
     <AppContext.Provider value={{ state, dispatch }}>
